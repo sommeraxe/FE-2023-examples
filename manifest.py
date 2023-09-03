42edfc9e01d32802ff6420ae182707dd568590ca
Adithya Somaraj <adithya.bs2001@gmail.com>
2023-09-03 18:31:03 +0530

    Update manifest.py

--- manifest.py
+++ manifest.py
@@ -6,14 +6,14 @@ import os
 sif_id = os.path.join(os.pardir, 'dtk_sif.id')
 
 # The script is going to use this to store the downloaded schema file. Create 'download' directory or change to your preferred (existing) location.
-schema_file=os.path.join(os.path.expanduser('~'),"download/schema.json")
+schema_file=os.path.join(os.path.expanduser('~'),"EMOD/download/schema.json")
 
 # The script is going to use this to store the downloaded Eradication binary. Create 'download' directory or change to your preferred (existing) location.
-eradication_path=os.path.join(os.path.expanduser('~'),"download/Eradication")
+eradication_path=os.path.join(os.path.expanduser('~'),"EMOD/download/Eradication")
 
 # Create 'Assets' directory or change to a path you prefer. idmtools will upload files found here.
 assets_input_dir="Assets"
-plugins_folder = "download/reporter_plugins"
+plugins_folder = "EMOD/download/reporter_plugins"
 
 # This is where your inputs are located
 ROOT_DIR = os.path.abspath(os.path.dirname(__file__))
@@ -26,7 +26,7 @@ ep4_path="python_scripts"
 # This is where your simulations and outputs will be stored
 user = os.getlogin()
 user_dir= f'FE_{user}'
-job_directory = '/home/adithya/EMOD/projects/FE-2023-examples/experiments'
+job_directory = os.path.join('/home/adithya/EMOD/projects/FE-2023-examples', user_dir, '/experiments')
 os.makedirs(job_directory, exist_ok=True)
 
 # This is the path to the sisf image used to run EMOD
